2885e7b1a6e60787d5e2891d5e4b14f1015df52e
Carlos Jesus Cruz Cervantes <44915801+carlosjecrcdev@users.noreply.github.com>
2024-06-26 00:53:40 -0600

    Test: Adding project Unit Testing
    -Adding test unit for methods in services

---
+++ ExpensesApi.Test/Providers/TestDbAsyncQueryProvider.cs
@@ -0,0 +1,43 @@
+﻿
+using ExpensesApi.Test.Providers;
+using Microsoft.EntityFrameworkCore.Query;
+using System.Collections;
+using System.Linq.Expressions;
+
+namespace ExpensesApi.Test.Helpers
+{  
+    public class TestDbAsyncQueryProvider<TEntity> : IAsyncQueryProvider
+    {
+        private readonly IQueryProvider _inner;
+
+        public TestDbAsyncQueryProvider(IQueryProvider inner)
+        {
+            _inner = inner ?? throw new ArgumentNullException(nameof(inner));
+        }
+
+        public IQueryable CreateQuery(Expression expression) => new TestDbAsyncEnumerable<TEntity>(_inner.CreateQuery<TEntity>(expression));
+
+        public IQueryable<TElement> CreateQuery<TElement>(Expression expression) => new TestDbAsyncEnumerable<TElement>(_inner.CreateQuery<TElement>(expression));
+
+        public object Execute(Expression expression) => _inner.Execute(expression);
+
+        public TResult Execute<TResult>(Expression expression) => _inner.Execute<TResult>(expression);
+
+        public IAsyncEnumerable<TResult> ExecuteAsync<TResult>(Expression expression)
+        {
+            var result = _inner.Execute<IEnumerable<TResult>>(expression);
+            return new TestDbAsyncEnumerable<TResult>(result);
+        }
+
+        public Task<TResult> ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            var result = _inner.Execute<TResult>(expression);
+            return Task.FromResult(result);
+        }
+
+        TResult IAsyncQueryProvider.ExecuteAsync<TResult>(Expression expression, CancellationToken cancellationToken)
+        {
+            return ExecuteAsync<TResult>(expression, cancellationToken).Result;
+        }
+    }
+}
